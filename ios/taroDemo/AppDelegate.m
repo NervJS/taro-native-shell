2e31b88100b1ef9e5ff8b5fcff66c1aaef05428c
Pines <spider.cs.nuc@gmail.com>
2019-06-12 11:26:11 +0800

    Update AppDelegate.m
    change jsBundleURLForBundleRoot path

--- ios/taroDemo/AppDelegate.m
+++ ios/taroDemo/AppDelegate.m
@@ -16,7 +16,7 @@
 {
   NSURL *jsCodeLocation;
 
-  jsCodeLocation = [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index" fallbackResource:nil];
+  jsCodeLocation = [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"rn_temp/index" fallbackResource:nil];
 
   RCTRootView *rootView = [[RCTRootView alloc] initWithBundleURL:jsCodeLocation
                                                       moduleName:@"taroDemo"
