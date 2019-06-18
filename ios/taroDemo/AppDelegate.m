f416b55be557b00deea8792a4e654e1162c304ce
Pines-Cheng <spider.cs.nuc@gmail.com>
2019-06-18 15:21:21 +0800

    feat: add expo-image-picker, react native version change to 0.55.4

--- ios/taroDemo/AppDelegate.m
+++ ios/taroDemo/AppDelegate.m
@@ -20,7 +20,7 @@
 {
   self.moduleRegistryAdapter = [[UMModuleRegistryAdapter alloc] initWithModuleRegistryProvider:[[UMModuleRegistryProvider alloc] init]];
   RCTBridge *bridge = [[RCTBridge alloc] initWithDelegate:self launchOptions:launchOptions];
-  RCTRootView *rootView = [[RCTRootView alloc] initWithBridge:bridge moduleName:@"taro-cli" initialProperties:nil];
+  RCTRootView *rootView = [[RCTRootView alloc] initWithBridge:bridge moduleName:@"taro-rn" initialProperties:nil];
   rootView.backgroundColor = [[UIColor alloc] initWithRed:1.0f green:1.0f blue:1.0f alpha:1];
 
   self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
