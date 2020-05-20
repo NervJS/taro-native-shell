6be2bd7d9f8de4a1a2a2c0082f5a9f0cd596debd
Pines-Cheng <spider.cs.nuc@gmail.com>
2020-05-20 16:40:05 +0800

    feat: add react-native-webview

--- android/app/src/main/java/com/tarodemo/MainApplication.java
+++ android/app/src/main/java/com/tarodemo/MainApplication.java
@@ -3,6 +3,9 @@ package com.tarodemo;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.airbnb.android.react.maps.MapsPackage;
+import com.imagepicker.ImagePickerPackage;
+import com.reactnative.ivpusic.imagepicker.PickerPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
@@ -29,6 +32,9 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new MapsPackage(),
+            new ImagePickerPackage(),
+            new PickerPackage(),
               new ModuleRegistryAdapter(mModuleRegistryProvider)
       );
     }
