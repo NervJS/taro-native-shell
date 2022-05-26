ac8e5489623ea703450106d84071e73c55bc77f8
chenzhiqing01 <zombieczq@gmail.com>
2022-05-26 19:13:56 +0800

    update

---
+++ src/pages/index/b.tsx
@@ -0,0 +1,5 @@
+import {View} from '@tarojs/components'
+
+export default function PageB () {
+    return <View>3333</View>
+}
--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -1,5 +1,6 @@
 import { Component } from 'react'
-import { View, Text } from '@tarojs/components'
+import { View, Text, Button, Video } from '@tarojs/components'
+import {scanCode,navigateTo} from '@tarojs/taro'
 import './index.scss'
 
 export default class Index extends Component {
@@ -12,10 +13,23 @@ export default class Index extends Component {
 
   componentDidHide () { }
 
+  handleClick() {
+    scanCode({}).then(console.log)
+  }
+
+  handleClick2() {
+    navigateTo({
+      url: '/pages/index/b'
+    })
+  }
+
   render () {
     return (
       <View className='index'>
-        <Text>Hello world!</Text>
+        <Text>Hello world!!!</Text>
+        <Button onClick={this.handleClick}>aaaa</Button>
+        <Button onClick={this.handleClick2}>bbbbb</Button>
+        <Video autoplay showPlayBtn={false} src='https://wos.anjukestatic.com/nrlKjIhGnEO/video/4a13605e-d2af-11ec-a27e-4ab7ef9771a0.mp4' />
       </View>
     )
   }
