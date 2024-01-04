b09d32217a412b3e71f9afd3f070041ff61b0436
tony chen <tony.zq.chen@gmail.com>
2024-01-04 18:10:02 +0800

    init 4.0.0-canary.6

---
+++ src/pages/index/index.tsx
@@ -0,0 +1,23 @@
+import { Component, PropsWithChildren } from 'react'
+import { View, Text } from '@tarojs/components'
+
+import './index.scss'
+
+export default class Index extends Component<PropsWithChildren> {
+
+  componentDidMount () { }
+
+  componentWillUnmount () { }
+
+  componentDidShow () { }
+
+  componentDidHide () { }
+
+  render () {
+    return (
+      <View className='index'>
+        <Text>Hello world!</Text>
+      </View>
+    )
+  }
+}
