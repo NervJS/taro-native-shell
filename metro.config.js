cdf8cc8265632e145e9c4c24aca67004b74d8b34
tony chen <tony.zq.chen@gmail.com>
2024-11-11 14:57:51 +0800

    add expo 52 & fix pnpm

--- metro.config.js
+++ metro.config.js
@@ -1,4 +1,5 @@
-const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
+const { getDefaultConfig } = require('expo/metro-config');
+const { mergeConfig } = require('@react-native/metro-config');
 
 /**
  * Metro configuration
